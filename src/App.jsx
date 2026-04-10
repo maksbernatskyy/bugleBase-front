a9ae6b83287d4ab26479bcfae6da2981ddc31628
maksbernatskyy <maksbernatskyy@gmail.com>
2026-04-10 15:37:25 +0200

    feat: set up routing with BrowserRouter and add HomePage component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
-function App() {
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+
+import HomePage from "./pages/HomePage"
+import DefaultLayout from "./components/DefaultLayout"
+
+export default function App() {
 
   return (
     <>
-      
+    <BrowserRouter>
+    <Routes>
+      <Route element={<DefaultLayout />}>
+        <Route index element={<HomePage />}/>
+      </Route>
+    </Routes>
+    </BrowserRouter>
     </>
   )
 }
-
-export default App
