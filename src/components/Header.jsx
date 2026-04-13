9a29631042d99eb0780b9dda8b5791624239087c
maksbernatskyy <maksbernatskyy@gmail.com>
2026-04-13 13:01:48 +0200

    add logo and color palette

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,11 @@
-
+import logo from '../assets/logo.png'
 
 export default function Header() {
     return (
         <>
         <header>
             <div>
-                
+                <img id='logo' src={logo} alt="Logo" />
             </div>
         </header>
         </>
